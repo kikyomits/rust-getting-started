0624862849d6eb7ad5ea72a05227555ede7c7145
kikyomits <mkikyotani@gmail.com>
2023-03-13 01:30:50 +1100

    fix: add endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use ferris_says::say;
-use rocket::http::SameSite::Strict;
-use std::io::stdin;
+use rocket::tokio::task::spawn_blocking;
+use rocket::tokio::time::{sleep, Duration};
+use std::io;
 
-fn main() {
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    stdin().read_line(&mut guess).expect("TODO: panic message");
-    println!("You guessed: {guess}");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello World"
+}
+
+#[get("/blocking_task")]
+async fn blocking_task() -> io::Result<Vec<u8>> {
+    // In a real app, use rocket::fs::NamedFile or tokio::fs::File.
+    let vec = spawn_blocking(|| std::fs::read("data.txt"))
+        .await
+        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+
+    Ok(vec)
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/hello", routes![index])
+        .mount("/", routes![blocking_task])
+        .mount("/", routes![delay])
+        .launch()
+        .await?;
+
+    Ok(())
 }
